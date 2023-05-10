a12a59b404847feca8d557daf640eeb1d181f662
tristanH77 <tristanhartman77@gmail.com>
2023-05-10 15:58:32 -0400

    sending right form data

--- src/components/AddPostForm/AddPostForm.jsx
+++ src/components/AddPostForm/AddPostForm.jsx
@@ -1,86 +1,93 @@
-// import { useState } from 'react'
 
-// import { Button, Form, Segment} from 'semantic-ui-react'
-
-// export default function AddPostForm({handleAddPost}){
-
-// 	const [caption, setCaption] = useState('')
-
-// 	const [selectedFile, setSelectedFile] = useState('')
-
-// 	function handleChange(e){
-// 		setCaption(e.target.value)
-// 	}
-
-// 	function handleFileInput(e){
-// 		setSelectedFile(e.target.files[0])
-// 	}
-
-// 	function handleSubmit(e){
-// 		e.preventDefault();
-// 		const formData = new FormData();
-// 		formData.append('caption', caption);
-// 		formData.append('photo', selectedFile);
-
-// 		handleAddPost(formData);
-
-// 	}
-
-// 	return (
-// 		<Segment>
-// 			<Form onSubmit={handleSubmit}>
-// 				<Form.Input 
-// 					placeholder='Add Post'
-// 					required
-// 					name="caption"
-// 					onChange={handleChange}
-// 				/>
-// 				<Form.Input 
-// 					type='file'
-// 					placeholder="upload image"
-// 					onChange={handleFileInput}
-// 				/>
-// 				<Button type="submit">Add Post</Button>
-// 			</Form>
-// 		</Segment>
-// 	)
-// }
 import React, { useState } from 'react';
+import '../AddPostForm/AddPostForm.css'
+import { Button, Form, Segment} from 'semantic-ui-react'
+import * as postsApi from '../../utils/postApi'
 
 const AddPostForm = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [imageUrl, setImageUrl] = useState('');
+  const [image2Url, setImage2Url] = useState('');
+  const [selectedImage, setSelectedImage] = useState('');
+  const [caption, setCaption] = useState('')
   const API_KEY = '8pFeuVJ1uZecQFD1COzwzfXo5cweGlqOdEQ3RaRT';
-  console.log(imageUrl)
-  const fetch_url = "https://images-api.nasa.gov/search?q=";
+  
+  const handleClick = (imgUrl) => {
+    setSelectedImage(imgUrl);
+  };
+  function handleChange(e){
+    setCaption(e.target.value)
+}
 
 
-  const handleSubmit = async (event) => {
+
+  const handleSearch = async (event) => {
     event.preventDefault();
     try {
         const response = await fetch(`https://images-api.nasa.gov/search?q=${searchQuery}`);
         const data = await response.json();
-      console.log(data);
+        console.log(data);
+        
+
       if (data.collection.items.length > 0) {
-        setImageUrl(data.collection.items[0].links[0].href);
+        setImageUrl(data.collection.items[0].links[0].href);        
         console.log(imageUrl)
+        console.log(data.collection.items[1].links[0].href, '<--- second in array');
+        setImage2Url(data.collection.items[1].links[0].href)
+        console.log(image2Url, 'image 2')
       }
     } catch (error) {
       console.log(error);
     }
   };
+  async function handleAddPost(post) {
+    try {
+      const responseData = await postsApi.create(post); 
+      console.log(responseData, " response from the server");
+    //   setPosts([responseData.data, ...posts]); 
+        console.log(post.append, 'post no error')
+    } catch (err) {
+        console.log(post, 'post error')
+      console.log(err, " error in addPost");
+    }
+  }
+  function handleSubmit(e){
+    e.preventDefault();
+    const formData = {
+        caption: caption,
+        photoUrl: selectedImage,
+    }
+
+    handleAddPost(formData); 
+    console.log(selectedImage, 'submit working')
+
+}
+
+
   
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSearch}>
         <label>
-          Search Query:
+          Search Nasa's Image Library:
           <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
         </label>
         <button type="submit">Search</button>
+        <div>
+        {imageUrl && <img src={imageUrl} className='returnImage' alt="Nasa Library Image" onClick={() => handleClick(imageUrl)} />}
+        {image2Url && <img src={image2Url} className='returnImage' alt="Nasa Library Image2" onClick={() => handleClick(image2Url)}/>}
+        {selectedImage && <img src={selectedImage} className='returnImage' alt="Selected Image" />}</div>
+
       </form>
-      {imageUrl && <img src={imageUrl} alt="Nasa Library Image" />}
+     <form onSubmit={handleSubmit}>
+        <Form.Input 
+		    placeholder='Caption for this Image'
+		    name="caption"
+		    onChange={handleChange}/>
+        <Button type="submit">Add Post</Button>
+     </form>
+
     </div>
   );
 };
