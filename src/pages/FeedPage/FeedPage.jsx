7363ea03914cef3afd669958ca3ba2973881b88a
tristanH77 <tristanhartman77@gmail.com>
2023-05-14 18:17:31 -0400

    style home page

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -4,7 +4,6 @@ import tokenService from "../../utils/tokenService";
 import { useState, useEffect } from "react";
 
 import PageHeader from "../../components/Header/Header";
-import AddPostForm from "../../components/AddPostForm/AddPostForm";
 import PostDisplay from "../../components/PostDisplay/PostDisplay";
 import Loader from "../../components/Loader/Loader";
 import * as postsApi from "../../utils/postApi";
@@ -18,19 +17,6 @@ export default function FeedPage({loggedUser}) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
 
-    async function handleAddPost(post) {
-      try {
-        setLoading(true);
-        const responseData = await postsApi.create(post); 
-        console.log(responseData, " response from the server");
-        setPosts([responseData.data, ...posts]); 
-        setLoading(false);
-      } catch (err) {
-        setLoading(false);
-        console.log(err, " error in addPost");
-        setError("Error creating a post, please try again");
-      }
-    }
     async function getPosts() {
       try {
         const response = await postsApi.getAll();
