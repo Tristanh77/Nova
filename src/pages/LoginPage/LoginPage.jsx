8849161fb235173745a4dc030bbc35254f565603
Tristanh77 <tristanhartman77@gmail.com>
2024-08-06 18:36:33 -0400

    fix login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -35,7 +35,7 @@ export default function LoginPage(props) {
     try {
       await userService.login(state);
       props.handleSignUpOrLogin();
-      navigate("/");
+      navigate("/feed");
     } catch (err) {
       setError(err.message);
     }
